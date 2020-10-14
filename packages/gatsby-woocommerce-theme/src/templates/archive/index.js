609a270b45d403b144bbb031f8b54e67ed753a54
Hasan Moboarak <hsncool8@gmail.com>
2020-10-14 15:28:52 +0600

    test

--- packages/gatsby-woocommerce-theme/src/templates/archive/index.js
+++ packages/gatsby-woocommerce-theme/src/templates/archive/index.js
@@ -3,8 +3,8 @@ import Search from "../../components/home/search";
 import { isEmpty } from 'lodash';
 import Layout from "../../components/layout";
 import Carousel from "../../components/home/carousel";
-import SEO from "../../components/seo";
-import { getOgImage } from "../../utils/functions";
+// import SEO from "../../components/seo";
+// import { getOgImage } from "../../utils/functions";
 
 const ArchivePage = ( props ) => {
 
@@ -22,13 +22,13 @@ const ArchivePage = ( props ) => {
 			{
 				! isEmpty( props.pageContext ) ? (
 					<>
-						<SEO
+						{/* <SEO
 							title={ name }
 							seoData={ seo }
 							uri={ uri }
 							header={ { siteTitle: 'Gatsby WooCommerce Theme' } }
 							openGraphImage={ getOgImage( seo ) }
-						/>
+						/> */}
 						<Carousel categories={ categories }/>
 						<Search
 							products={ products }
--- packages/gatsby-woocommerce-theme/src/templates/front-page/index.js
+++ packages/gatsby-woocommerce-theme/src/templates/front-page/index.js
@@ -3,14 +3,14 @@ import Search from "../../components/home/search";
 import { isEmpty } from 'lodash';
 import Layout from "../../components/layout";
 import Carousel from "../../components/home/carousel";
-import SEO from "../../components/seo";
-import { getOgImage } from "../../utils/functions";
+// import SEO from "../../components/seo";
+// import { getOgImage } from "../../utils/functions";
 
 const FrontPage = ( props ) => {
 	
 	const {
 		      pageContext: {
-			      page: { title, seo, uri },
+			    //   page: { title, seo, uri },
 			      categories,
 			      categoryName,
 			      postSearchData: { products, options }
@@ -23,13 +23,13 @@ const FrontPage = ( props ) => {
 			{
 				! isEmpty( props.pageContext ) ? (
 					<>
-						<SEO
+						{/* <SEO
 							title={ title }
 							seoData={ seo }
 							uri={ uri }
 							header={ { siteTitle: 'Gatsby WooCommerce Theme' } }
 							openGraphImage={ getOgImage( seo ) }
-						/>
+						/> */}
 						<Carousel categories={ categories }/>
 						<Search
 							products={ products }
